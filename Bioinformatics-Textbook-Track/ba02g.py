a49de7abe8305932fb4ed44ef51a08cf0cc24fd2
takyam15 <u779052i@alumni.osaka-u.ac.jp>
2022-07-28 15:54:30 +0900

    solved several problems

--- Bioinformatics-Textbook-Track/ba02g.py
+++ Bioinformatics-Textbook-Track/ba02g.py
@@ -1,6 +1,9 @@
 # Implement GibbsSampler
 
 
+import numpy as np
+
+
 def read(file):
     with open(file) as f:
         k, t, n = f.readline().strip().split(' ')
@@ -24,8 +27,68 @@ def get_profile(motifs):
     return profile_matrix
 
 
-def main(file, export=True):
+def get_motifs(profile_matrix, motifs, n, string):
+    probs = {}
+    k = len(profile_matrix)
+    for i in range(len(string)-k+1):
+        pattern = string[i:i+k]
+        probs[pattern] = np.prod([profile_matrix[n][pattern[n]] for n in range(len(pattern))])
+    motifs.insert(n, [motif for motif in probs if probs[motif]==max(probs.values())][0])
+    return motifs
+
+
+def hamming_distance(string1, string2):
+    distance = 0
+    for s1, s2 in zip(string1, string2):
+        if s1 != s2:
+            distance += 1
+    return distance
+
+
+def generate_motif(motifs):
+    length = len(motifs[0])
+    pattern = ''
+    for i in range(length):
+        counts = {}
+        for motif in motifs:
+            s = motif[i]
+            counts[s] = counts.get(s, 0) + 1
+        pattern += [s for s in counts if counts[s]==max(counts.values())][0]
+    return pattern
+
+
+def score(motifs):
+    pattern = generate_motif(motifs)
+    distance = 0
+    for motif in motifs:
+        distance += hamming_distance(pattern, motif)
+    return distance
+
+
+def main(file, export=True, n_iter=20):
     k, t, n, strings = read(f'./dataset/{file}.txt')
+    motif_scores = {}
+    for _ in range(n_iter):
+        motifs = []
+        for string in strings:
+            i = np.random.randint(len(string)-k+1)
+            motifs.append(string[i:i+k])
+        best_motifs = motifs
+        for a in range(n):
+            j = np.random.randint(t)
+            motifs.pop(j)
+            profile_matrix = get_profile(motifs)
+            motifs = get_motifs(profile_matrix, motifs, j, strings[j])
+            if score(motifs) < score(best_motifs):
+                best_motifs = motifs
+        motif_scores['\n'.join(best_motifs)] = score(best_motifs)
+    output = [
+        selected_motifs for selected_motifs in motif_scores if motif_scores[selected_motifs]==min(motif_scores.values())
+    ][0]
+    print(output)
+    if export:
+        with open(f'./submission/{file}.txt', mode='w') as f:
+            f.write(output)
 
 
 if __name__ == '__main__':
--- Bioinformatics-Textbook-Track/ba03f.py
+++ Bioinformatics-Textbook-Track/ba03f.py
@@ -1,3 +1,89 @@
 # Find an Eulerian Cycle in a Graph
 
 
+def read(file):
+    with open(file) as f:
+        graph = f.read().strip().split('\n')
+    adjacency_list_raw = [
+        adjacency.split('->') for adjacency in graph
+    ]
+    adjacency_list = []
+    for adjacency in adjacency_list_raw:
+        if ',' in adjacency[1]:
+            nodes = adjacency[1].split(',')
+            adjacency_list.extend(
+                [(adjacency[0].strip(), node.strip()) for node in nodes]
+            )
+        else:
+            adjacency_list.append(
+                (adjacency[0].strip(), adjacency[1].strip())
+            )
+    return adjacency_list
+
+
+def add_edge(cycle, adjacency):
+    if cycle[-1] == adjacency[0]:
+        cycle.append(adjacency[1])
+        return cycle
+
+
+def find_cycle(cycle, adjacency_list):
+    if len(adjacency_list) == 1:
+        if cycle[-1] == adjacency_list[0][1]:
+            return cycle + adjacency_list[-1]
+    else:
+        candidates = [
+            adj for adj in adjacency_list if adj[0] == cycle[-1]
+        ]
+        if candidates:
+            for candidate in candidates:
+                cycle = find_cycle(cycle.append(candidate[1]), adjacency_list.remove(candidate))
+                if cycle:
+                    return cycle
+
+
+def form_cycle(adjacency_list):
+    for adjacency in adjacency_list:
+        adjacency_list_loop = adjacency_list.copy()
+        cycle = '->'.join(adjacency)
+        #print(cycle)
+        adjacency_list_loop.remove(adjacency)
+        previous_adjacency = adjacency
+        #print(adjacency_list)
+        while len(adjacency_list_loop):
+            next_adjacency = [
+                edge for edge in adjacency_list_loop if edge[0] == previous_adjacency[1]
+            ]
+            if next_adjacency:  # TODO: ここをどうするか
+                cycle_tmp = cycle
+                adjacency_list_tmp = adjacency_list_loop.copy()
+                for adj in next_adjacency:
+                    cycle_tmp += '->' + adj[1]
+                    #print(cycle)
+                    previous_adjacency = adj
+                    adjacency_list_tmp.remove(adj)
+                    #print(adjacency_list)
+            else:
+                break
+        if not len(adjacency_list_loop):
+            return cycle
+
+
+def main(file, export=True):
+    adjacency_list = read(f'./dataset/{file}.txt')
+    #print(adjacency_list)
+    #cycle = form_cycle(adjacency_list)
+    for adj in adjacency_list:
+        start = adj
+        cycle = find_cycle(start, )
+    cycle = find_cycle([], adjacency_list)
+    output = '->'.join(cycle)
+    print(output)
+    if export:
+        with open(f'./submission/{file}.txt', mode='w') as f:
+            f.write(output)
+
+
+if __name__ == '__main__':
+    main('sample_rosalind_ba3f')
+    main('rosalind_ba3f')
